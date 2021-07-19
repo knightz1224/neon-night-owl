67652b1380802771433393b6a827d21ae4593413
WinZ <knightz1224@gmail.com>
2021-07-19 12:40:06 +0800

    newcolor

--- lua/neon/init.lua
+++ lua/neon/init.lua
@@ -4,22 +4,22 @@ local cfg = require("neon.config").styles
 local neon = {}
 
 local function set_terminal_colors()
-    vim.g.terminal_color_0 = c.bg2
-    vim.g.terminal_color_1 = c.cyan
-    vim.g.terminal_color_2 = c.green
-    vim.g.terminal_color_3 = c.blue
-    vim.g.terminal_color_4 = c.yellow
-    vim.g.terminal_color_5 = c.purple
-    vim.g.terminal_color_6 = c.blue
-    vim.g.terminal_color_7 = c.blue
-    vim.g.terminal_color_8 = c.yellow
-    vim.g.terminal_color_9 = c.red
-    vim.g.terminal_color_10 = c.green
-    vim.g.terminal_color_11 = c.cyan
-    vim.g.terminal_color_12 = c.dark_cyan
+    vim.g.terminal_color_0  = c.regal_blue
+    vim.g.terminal_color_1  = c.red
+    vim.g.terminal_color_2  = c.green
+    vim.g.terminal_color_3  = c.yellow
+    vim.g.terminal_color_4  = c.blue
+    vim.g.terminal_color_5  = c.violet
+    vim.g.terminal_color_6  = c.turquoise
+    vim.g.terminal_color_7  = c.cadet_blue
+    vim.g.terminal_color_8  = c.grey_blue
+    vim.g.terminal_color_9  = c.watermelon
+    vim.g.terminal_color_10 = c.emerald
+    vim.g.terminal_color_11 = c.tan
+    vim.g.terminal_color_12 = c.blue
     vim.g.terminal_color_13 = c.purple
-    vim.g.terminal_color_14 = c.green
-    vim.g.terminal_color_15 = c.blue
+    vim.g.terminal_color_14 = c.turquoise
+    vim.g.terminal_color_15 = c.white_blue
     if vim.g.neon_transparent then
         c.bg0 = "NONE"
     end
