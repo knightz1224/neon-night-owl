692e1343602d39f2b6b438845c15e199ce9d1978
WinZ <kngihtz1224@gmail.com>
2021-09-13 22:58:10 +0800

    new color

--- lua/lualine/themes/neon.lua
+++ lua/lualine/themes/neon.lua
@@ -1,13 +1,33 @@
-local colors = require("neon.colors")
+local colors = {
+    bg0 = '#011627',
+    bg3 = '#011627',
+    -- bg = '#292D38',
+    yellow = '#ECBE7B',
+    dark_yellow = '#D7BA7D',
+    cyan = '#008080',
+    green = '#608B4E',
+    light_green = '#B5CEA8',
+    string_orange = '#CE9178',
+    orange = '#FF8800',
+    purple = '#C586C0',
+    magenta = '#D16D9E',
+    grey = '#858585',
+    blue = '#51AFEF',
+    vivid_blue = '#4FC1FF',
+    light_blue = '#9CDCFE',
+    red = '#EC5F67',
+    error_red = '#F44747',
+    info_yellow = '#FFCC66'
+}
 
 local neon = {
     visual = {
-        a = {fg = colors.bg0, bg = colors.blue, gui = "bold"},
-        b = {fg = colors.cyan, bg = colors.bg3}
+        a = {fg = colors.bg0, bg = colors.purple, gui = "bold"},
+        b = {fg = colors.blue, bg = colors.bg3}
     },
     replace = {
-        a = {fg = colors.bg0, bg = colors.disabled, gui = "bold"},
-        b = {fg = colors.purple, bg = colors.bg3}
+        a = {fg = colors.bg0, bg = colors.cyan, gui = "bold"},
+        b = {fg = colors.blue, bg = colors.bg3}
     },
     inactive = {
         a = {fg = colors.fg, bg = colors.bg3, gui = "bold"},
@@ -15,18 +35,18 @@ local neon = {
         c = {fg = colors.fg, bg = colors.bg3}
     },
     normal = {
-        a = {fg = colors.bg0, bg = colors.dark_cyan, gui = "bold"},
+        a = {fg = colors.bg0, bg = colors.blue, gui = "bold"},
         b = {fg = colors.blue, bg = colors.bg3},
-        c = {fg = colors.fg, bg = colors.bg3}
+        c = {fg = colors.blue, bg = colors.bg3}
     },
     insert = {
-        a = {fg = colors.bg0, bg = colors.purple, gui = "bold"},
-        b = {fg = colors.green, bg = colors.bg3}
+        a = {fg = colors.bg0, bg = colors.green, gui = "bold"},
+        b = {fg = colors.blue, bg = colors.bg3}
     },
     command = {
-        a = {fg = colors.bg0, bg = colors.red, gui = "bold"},
-        b = {fg = colors.green, bg = colors.bg3}
-    }
+        a = {fg = colors.bg0, bg = colors.magenta, gui = "bold"},
+        b = {fg = colors.blue, bg = colors.bg3}
+    },
 }
 
 return neon
